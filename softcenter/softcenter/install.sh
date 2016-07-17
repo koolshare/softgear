9062f8f4f33076e21a0992d74793eb57723c35f3
janson <gandancing@gmail.com>
2016-07-17 23:53:32 +0800

    todo for softcenter

--- softcenter/softcenter/install.sh
+++ softcenter/softcenter/install.sh
@@ -3,18 +3,15 @@
 softcenter_install() {
 	if [ -d "/tmp/softcenter" ]; then
 		cp -rf /tmp/softcenter/webs/* /koolshare/webs
-		cp -rf /tmp/softcenter/res/* /koolshare/res/
-		cp -rf /tmp/softcenter/bin/* /koolshare/bin/
-		cp -rf /tmp/softcenter/perp /koolshare/
+		mkdir -p /koolshare/webs/res
+		cp -rf /tmp/softcenter/res/* /koolshare/webs/res/
 		cp -rf /tmp/softcenter/scripts /koolshare/
-		chmod 755 /koolshare/bin/*
-		chmod 755 /koolshare/perp/*
-		chmod 755 /koolshare/perp/.boot/*
-		chmod 755 /koolshare/perp/.control/*
-		chmod 755 /koolshare/perp/adm/*
 		rm -rf /tmp/softcenter
 		if [ ! -f "/koolshare/init.d/S10Softcenter.sh" ]; then
-		ln -sf /koolshare/scripts/app_install.sh /koolshare/init.d/S10Softcenter.sh
+		ln -sf /koolshare/scripts/ks_app_install.sh /koolshare/init.d/S10Softcenter.sh
+		fi
+		if [ ! -f "/koolshare/init.d/S10Softcenter.sh" ]; then
+		ln -sf /koolshare/scripts/ks_app_install.sh /koolshare/scripts/ks_app_remove.sh
 		fi
 	fi
 }
