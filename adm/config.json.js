482f298dad6caeace4f926cd71bfcf9a80c5edfd
janson <gandancing@gmail.com>
2016-07-17 22:14:15 +0800

    todo for softcenter

---
+++ adm/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.5",
+"md5":"18db0cc360e29c3a2619dd88fad2264f",
+"home_url":"Module_adm.asp",
+"title":"阿呆猫",
+"description":"去广告，看疗效~",
+"build_date":"2016-07-17_12:55:33"
+}
---
+++ softcenter/app.json.js
@@ -0,0 +1,18 @@
+{
+    "apps": [
+        {
+            "build_date": "2016-07-17_12:55:33", 
+            "description": "去广告，看疗效~", 
+            "home_url": "Module_adm.asp", 
+            "md5": "18db0cc360e29c3a2619dd88fad2264f", 
+            "name": "adm", 
+            "tar_url": "adm/adm.tar.gz", 
+            "title": "阿呆猫", 
+            "version": "1.5"
+        }
+    ], 
+    "home_url": "http://netgear.ngrok.wang:5000", 
+    "md5": "08fa585e685e21a90af672691a888879", 
+    "tar_url": "softcenter/softcenter.tar.gz", 
+    "version": "1.0.7"
+}
